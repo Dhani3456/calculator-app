3f0c9670b1b410a520e7af13b5e4bb0cebbdea75
Dhani3456 <ahmadfauziramadhani@students.amikom.ac.id>
2023-03-15 12:09:44 +0700

    manmbahkan perubahan calculator

---
+++ calculator.cs
@@ -0,0 +1,13 @@
+class Calculator
+{
+	static void Main(string[]args)
+	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Pembagian: {0} + {1} = {2}", a, b, Penambahan(a, b));
+	}
+	static int penambahan(int a, int b)
+	{
+		return a + b 
+}
